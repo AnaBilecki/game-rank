232536b1d6e360d21d8b95733f0588f04e43fddb
Ana Carolina Bilecki <anacarolina255@gmail.com>
2025-03-30 13:58:56 -0300

    add database schema

---
+++ database/script.sql
@@ -0,0 +1,39 @@
+CREATE TABLE user (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    image VARCHAR(200),
+    bio TEXT,
+    token VARCHAR(200)
+);
+
+CREATE TABLE category (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE movie (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+    description TEXT,
+    image VARCHAR(200),
+    trailer VARCHAR(150),
+    category_id INT(11) UNSIGNED,
+    user_id INT(11) UNSIGNED,
+    
+    FOREIGN KEY(category_id) REFERENCES category(id),
+    FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+CREATE TABLE review (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    rating INT NOT NULL,
+    review TEXT,
+    user_id INT(11) UNSIGNED,
+    movie_id INT(11) UNSIGNED,
+    
+    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(movie_id) REFERENCES movie(id)
+);
