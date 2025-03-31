7908369bb6efd23b5b6d299c1ce225821b1c1c69
Ana Carolina Bilecki <anacarolina255@gmail.com>
2025-03-30 21:26:18 -0300

    add footer template

---
+++ templates/footer.php
@@ -0,0 +1,18 @@
+<footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <p>&copy; 2025 Game Rank</p>
+    </footer>
+</body>
+</html>
