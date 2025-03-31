07b9e040703f54a5dcfa00dde309c426c104976f
Ana Carolina Bilecki <anacarolina255@gmail.com>
2025-03-30 21:28:31 -0300

    include header and footer templates in index page

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <?php
-
-    include_once("config/db.php");
-    include_once("config/globals.php");
-
+    include_once("templates/header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GameRank</title>
-    <link rel="icon" href="img/joystick.png" type="image/png"/>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-    <h1>GameRank</h1>
-</body>
-</html>
+    <div id="main-container">
+    </div>
+<?php
+    include_once("templates/footer.php");
+?> 
