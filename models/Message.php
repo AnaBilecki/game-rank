6c395d65ad8472403011cf430b0bbc3296d63c91
Ana Carolina Bilecki <anacarolina255@gmail.com>
2025-04-03 21:57:53 -0300

    create message model

---
+++ models/Message.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+class Message
+{
+
+    private $url;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function setMessage($message, $type, $redirect = "index.php")
+    {
+        $_SESSION["message"] = $message;
+        $_SESSION["type"] = $type;
+
+        if ($redirect != "back") {
+            header("Location: $this->url" . $redirect);
+        } else {
+            header("Location: " . $_SERVER["HTTP_REFERER"]);
+        }
+    }
+
+    public function getMessage()
+    {
+        if (!empty($_SESSION["message"])) {
+            return [
+                "message" => $_SESSION["message"],
+                "type" => $_SESSION["type"]
+            ];
+        } else {
+            return false;
+        }
+    }
+
+    public function clearMessage()
+    {
+        $_SESSION["message"] = "";
+        $_SESSION["type"] = "";
+    }
+}
